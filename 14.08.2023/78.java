ce764e3a6510460da20e25a15992d8dcacba4017
Dishit Shah <dishit.s@ahduni.edu.in>
2023-08-18 14:58:00 +0530

    Modified with recursion

--- 14.08.2023/78.java
+++ 14.08.2023/78.java
@@ -3,17 +3,20 @@ import java.util.List;
 
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> n = new ArrayList<>();
-        n.add(new ArrayList<>());
+        List<List<Integer>> result = new ArrayList<>();
+        generateSubsets(nums, 0, new ArrayList<>(), result);
+        return result;
+    }
 
-        for (int i : nums) {
-            int a = n.size();
-            for (int j = 0; j < a; j++) {
-                List<Integer> ans = new ArrayList<>(n.get(j));
-                ans.add(i);
-                n.add(ans);
-            }
+    private void generateSubsets(int[] nums, int index, List<Integer> current, List<List<Integer>> result) {
+        if (index == nums.length) {
+            result.add(new ArrayList<>(current));
+            return;
         }
-        return n;
+
+        generateSubsets(nums, index + 1, current, result); 
+        current.add(nums[index]);
+        generateSubsets(nums, index + 1, current, result); 
+        current.remove(current.size() - 1); 
     }
 }
